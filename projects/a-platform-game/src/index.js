e92443b262ab536966dad9091213823ee97a2162
Dillon Morse <dillon.arthur.morse@outlook.com>
2019-08-04 17:23:31 -0400

    canvas display engine for platform game

--- projects/a-platform-game/src/index.js
+++ projects/a-platform-game/src/index.js
@@ -1,5 +1,5 @@
-const {DOMDisplay} = require("./display");
+const {CanvasDisplay} = require("./display");
 const {GAME_LEVELS} = require("./levels");
 const {runGame} = require("./game");
 
-runGame(GAME_LEVELS, DOMDisplay);
+runGame(GAME_LEVELS, CanvasDisplay);
